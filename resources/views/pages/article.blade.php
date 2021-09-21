2fa69dcb05c4bfcf1ab1ffae1a40c6d33d803725
chris De Groef <chris.degroef@gmail.com>
2021-09-21 14:13:18 +0200

    Laravel : middleware part 1

---
+++ resources/views/pages/article.blade.php
@@ -0,0 +1,7 @@
+@extends('template.template')
+
+@section('content')
+
+    <p>vous êtes connectés, vous avez donc accès à cette blade</p>
+
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,4 @@
+<div class="">
+    <button><a href="{{ route('welcome') }}">Accueil</a></button>
+    <button><a href="{{ route('article') }}">Articles</a></button>
+</div>
